8996188ead851c04cdc500c22b1ade48880b4b71
Megan Frisella <mmfrisella@wpi.edu>
2019-07-25 16:51:47 -0400

    change

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import image_query
 import token_to_idf_db
+from gensim.models.keyedvectors import KeyedVectors
+import pickle
+
+# load glove 50
+glove50 = KeyedVectors.load_word2vec_format("glove.6B.50d.txt.w2v", binary=False)
+
+# load image dataset
+f = open("database.p", "rb")
+database = pickle.load(f)
+f.close()
 
 
 def run():
@@ -17,9 +27,8 @@ def run():
     text = input("Search: ")
     input_string = text
 
-    glove50 = image_query.load_glove()
     words_to_idfs = token_to_idf_db.import_token_to_idf()
 
     embedded_text = image_query.se_text(input_string, words_to_idfs, glove50)
-    urls = image_query.query(embedded_text, 10)
+    urls = image_query.query(embedded_text, 10, database)
     image_query.display_images(urls)

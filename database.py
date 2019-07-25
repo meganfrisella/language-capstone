70efa5a13408fba22b3aa78f64d69e5cb1ee519f
Megan Frisella <mmfrisella@wpi.edu>
2019-07-25 16:03:49 -0400

    moved se image to database

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import embeddings
 import pickle
 import json
+import numpy as np
 
 
 def load_coco_features():
@@ -44,6 +45,33 @@ def load_coco_metadata():
     return coco_metadata
 
 # Lilian is awesome!!!
+
+
+def se_image(image_features):
+    """
+    Description
+
+    Parameters
+    ----------
+    image_features: np.array, shape=(1, 512)
+        Image feature array from COCO dataset
+
+    M: np.array, shape=(512, 50)
+        Trained parameter for embedding image feature array
+
+    b: np.int
+        Trained bias parameter for embedding image feature array
+
+    Returns
+    -------
+    np.array, shape=(1, 50)
+        Image embedding
+
+    """
+    # M and b are made up, need to get from Christian
+    M = np.ones((512, 50))
+    b = 1
+    return image_features @ M + b
 
 
 def image_dataset():

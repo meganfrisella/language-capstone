6819f67a8c60148d4f40edd01b5876b00990ec76
Megan Frisella <mmfrisella@wpi.edu>
2019-07-25 14:31:30 -0400

    querying

--- image_embedding.py
+++ image_embedding.py
@@ -1,28 +1,6 @@
-import pickle
 
 
-def load_coco_images():
-    """
-    Loads the COCO dictionary from its pickle file.
-
-    Parameters
-    ----------
-
-    Returns
-    -------
-    dict
-        A dictionary that maps image IDs to (1, 512) image feature vectors
-
-    """
-
-    f = open("./resnet18_features.pkl", "rb")
-    coco_images = pickle.load(f)
-    f.close()
-
-    return coco_images
-
-
-def se_image(image_features, M, b):
+def se_image(image_features):
     """
     Description
 
@@ -43,4 +21,6 @@ def se_image(image_features, M, b):
         Image embedding
 
     """
+    M = # fill in here
+    b = # fill in here
     return image_features * M + b
